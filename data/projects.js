7a95d5cc3ae2f9e6402e101623e1630d65e1c7db
Sean OReilly <soreilly424@gmail.com>
2022-02-23 08:38:20 -0600

    command palette using headless ui

---
+++ data/projects.js
@@ -0,0 +1,14 @@
+export const projects = [
+  { id: 1, name: 'Onboarding', team: 'team 1', url: 'https://www.google.com' },
+  {
+    id: 2,
+    name: 'IOS Development',
+    team: 'team 2',
+    url: 'https://www.google.com',
+  },
+  { id: 3, name: 'Testing', team: 'team 3', url: 'https://www.google.com' },
+  { id: 4, name: 'something 4', team: 'team 4', url: 'https://www.google.com' },
+  { id: 5, name: 'something 5', team: 'team 5', url: 'https://www.google.com' },
+  { id: 6, name: 'something 6', team: 'team 6', url: 'https://www.google.com' },
+  { id: 7, name: 'something 7', team: 'team 7', url: 'https://www.google.com' },
+]
